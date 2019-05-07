5980d240cf7d802ec7791e792d37e7e1c4862ca0
Brownhu <1104524352@qq.com>
2019-05-07 13:12:10 +0800

    bug fix

--- src/router.ts
+++ src/router.ts
@@ -6,20 +6,12 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/paper-different/dist/index.html',
   routes: [
     {
       path: '/',
       name: 'Translate',
       component: Translate,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
   ],
 });
